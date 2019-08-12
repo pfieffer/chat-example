11d2852f68c08b8cc0c8fe4ab4141e4ca78e0f14
Ravi Garbuja Pun <cbpmgr@gmail.com>
2019-08-12 14:51:58 +0545

    feat: Complete broadcast type chat application

--- index.js
+++ index.js
@@ -11,9 +11,10 @@ io.on('connection', function (socket) {
     socket.on('disconnect', function () {
         console.log('user disconnected');
     });
-    socket.on('chat message', function(msg){
+    socket.on('chat message', function (msg) {
         console.log('message: ' + msg);
-      });
+        io.emit('chat message', msg);
+    });
 });
 
 http.listen(3000, function () {
